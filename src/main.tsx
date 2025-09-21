bc8618bf6815ebde8ba782ff0f6621c8af20905d
dpix69-art <dpix69@gmail.com>
2025-09-21 21:15:40 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { DictionaryEntry } from './pages/DictionaryEntry';
-import { Dictionary } from './pages/Dictionary';
-
-
 
 
 import './index.css';
@@ -13,7 +10,7 @@ import './index.css';
 import { Home } from './pages/Home';
 import { Level } from './pages/Level';
 import { Card } from './pages/Card';
-
+import { Dictionary } from './pages/Dictionary';
 import { Emails } from './pages/Emails';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -24,7 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/level/:moduleId" element={<Level />} />
         <Route path="/card/:moduleId/:cardId" element={<Card />} />
         <Route path="/dictionary" element={<Dictionary />} />
-
         <Route path="/dictionary/:entryId" element={<DictionaryEntry />} />
         <Route path="/emails" element={<Emails />} />
       </Routes>
