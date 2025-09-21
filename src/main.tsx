d33d687246856259e1ae14c9e088962beae48e7c
dpix69-art <dpix69@gmail.com>
2025-09-21 21:44:30 +0200

    ffff

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,7 @@ import './index.css';
 import { Home } from './pages/Home';
 import { Level } from './pages/Level';
 import { Card } from './pages/Card';
-import  Dictionary  from './pages/Dictionary';
-import { DictionaryCategory } from './pages/DictionaryCategory';
-import { DictionaryEntry } from './pages/DictionaryEntry';
+import { Dictionary } from './pages/Dictionary';
 import { Emails } from './pages/Emails';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -20,11 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Home />} />
         <Route path="/level/:moduleId" element={<Level />} />
         <Route path="/card/:moduleId/:cardId" element={<Card />} />
-
         <Route path="/dictionary" element={<Dictionary />} />
-        <Route path="/dictionary/category/:bucket" element={<DictionaryCategory />} />
-        <Route path="/dictionary/:entryId" element={<DictionaryEntry />} />
-
         <Route path="/emails" element={<Emails />} />
       </Routes>
     </HashRouter>
